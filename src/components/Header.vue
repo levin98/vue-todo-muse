4c52ef5e63b932e090b9380aa33fd381faad3691
Kelvin <kelvinlim_98@live.com>
2019-03-14 18:29:02 +0800

    Added header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <mu-appbar color="primary">
+    <mu-button icon slot="left"> <mu-icon value="menu"></mu-icon> </mu-button
+    >Dashboard
+    <mu-menu slot="right">
+      <mu-button flat>MENU</mu-button>
+      <mu-list slot="content">
+        <mu-list-item button>
+          <mu-list-item-content>
+            <mu-list-item-title>Menu Item 1</mu-list-item-title>
+          </mu-list-item-content>
+        </mu-list-item>
+        <mu-list-item button>
+          <mu-list-item-content>
+            <mu-list-item-title>Menu Item 2</mu-list-item-title>
+          </mu-list-item-content>
+        </mu-list-item>
+      </mu-list>
+    </mu-menu>
+  </mu-appbar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
