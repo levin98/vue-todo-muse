ece4d61fa9c2a8465afcfc2f1ce664ccec3a0fbb
Kelvin <kelvinlim_98@live.com>
2019-03-14 16:58:15 +0800

    Added muse-ui and imported muse-ui

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+// Import Muse-ui
+import MuseUI from "muse-ui";
+import "muse-ui/dist/muse-ui.css";
 
+Vue.use(MuseUI);
 Vue.config.productionTip = false;
 
 new Vue({
