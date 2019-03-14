c52882a0b06ce0b164d095acfa30cd18bb678794
Kelvin <kelvinlim_98@live.com>
2019-03-14 19:21:36 +0800

    Initial home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,23 @@
   <div class="home">
     <mu-card class="to-do-item">
       <mu-flex class="to-do-wrapper" justify-content="start">
-        <mu-flex class="to-do-checkbox" justify-content="start">
+        <mu-flex
+          class="to-do-checkbox"
+          justify-content="start"
+          align-self="center"
+        >
           <mu-checkbox></mu-checkbox>
         </mu-flex>
-        <mu-flex class="to-do-title" justify-content="start">
+        <mu-flex
+          class="to-do-title"
+          justify-content="start"
+          align-self="center"
+        >
           <span>Title</span>
         </mu-flex>
       </mu-flex>
     </mu-card>
-    <mu-button id="create" fab large color="primary">
+    <mu-button id="create" fab medium color="primary">
       <mu-icon value="add"></mu-icon>
     </mu-button>
   </div>
@@ -32,12 +40,25 @@ export default {
 
 #create {
   position: fixed;
-  bottom: 50px;
-  right: 50px;
+  bottom: 40px;
+  right: 40px;
 }
 
 .to-do-item {
   width: 100%;
-  vertical-align: middle;
+  height: 50px;
+}
+
+.to-do-wrapper {
+  width: 100%;
+  height: 50px;
+}
+
+.to-do-checkbox {
+  margin: 0px 20px;
+}
+
+.to-do-title {
+  margin: 0px 20px;
 }
 </style>
