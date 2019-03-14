5fdd6c669eb4dfed04fc3ebcf4164864e8cbe124
Kelvin <kelvinlim_98@live.com>
2019-03-14 17:09:21 +0800

    Added muse-ui theme and applied dark theme;

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
 // Import Muse-ui
 import MuseUI from "muse-ui";
 import "muse-ui/dist/muse-ui.css";
+
+// Import Muse-ui theme
+import theme from "muse-ui/lib/theme";
+
+// Use default dark theme
+theme.use("dark");
 
 Vue.use(MuseUI);
 Vue.config.productionTip = false;
