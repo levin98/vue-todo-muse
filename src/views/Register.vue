014373f8e51bf3c74f1f75714a7ac71ded0f197c
MSI\Kelvin <kelvinlim_98@live.com>
2019-03-15 16:16:37 +0800

    Added register

---
+++ src/views/Register.vue
@@ -0,0 +1,92 @@
+<template>
+  <mu-paper class="register-container" :z-depth="5">
+    <h1>Register</h1>
+    <mu-form
+      ref="form"
+      :model="registerForm"
+      :label-position="registerForm.labelPosition"
+      class="login-form"
+    >
+      <mu-form-item label="Username" prop="username" :rules="usernameRules">
+        <mu-text-field
+          v-model="registerForm.username"
+          prop="username"
+        ></mu-text-field>
+      </mu-form-item>
+      <mu-form-item label="Password" prop="password" :rules="passwordRules">
+        <mu-text-field
+          type="password"
+          v-model="registerForm.password"
+          prop="password"
+        ></mu-text-field>
+      </mu-form-item>
+      <mu-form-item>
+        <mu-button color="primary" @click="submit">Sign Up</mu-button>
+        <mu-button @click="clear">Reset</mu-button>
+      </mu-form-item>
+    </mu-form>
+    <router-link to="/login">
+      <mu-button color="primary" flat>Sign In</mu-button>
+    </router-link>
+  </mu-paper>
+</template>
+
+<script>
+export default {
+  name: "register",
+  data() {
+    return {
+      usernameRules: [
+        { validate: val => !!val, message: "Username must be filled in" },
+        {
+          validate: val => val.length >= 3,
+          message: "Username length greater than 3"
+        }
+      ],
+      passwordRules: [
+        { validate: val => !!val, message: "Password must be filled in" },
+        {
+          validate: val => val.length >= 3 && val.length <= 10,
+          message: "Password length must be greater than 3 and less than 10"
+        }
+      ],
+      registerForm: {
+        labelPosition: "top",
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      this.$refs.form.validate().then(result => {
+        //eslint-disable-next-line
+        console.log('form valid: ', result)
+      });
+    },
+    clear() {
+      this.$refs.form.clear();
+      this.registerForm = {
+        username: "",
+        password: ""
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+.register-container {
+  height: 50%;
+  width: 50%;
+  top: 25%;
+  left: 25%;
+  padding: 5%;
+  position: fixed;
+  border-radius: 5px;
+}
+
+div /deep/ .mu-form-item-label {
+  text-align: left;
+}
+</style>
